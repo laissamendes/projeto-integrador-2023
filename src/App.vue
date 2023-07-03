9a6052f76051dc5e50b8741e52588ee6c7fd363f
Laíssa Mendes <laissa.mendes2006@gmail.com>
2023-07-03 10:56:02 -0300

    mudança para vue e criação do bd

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { perguntas } from './assets/db.vue'
+</script>
+
+<template>
+  <div id="navegador">
+    <nav>
+      <div><img src="favicon.png" alt="" width="50px" /></div>
+      <div>
+        <h2>Inicio</h2>
+      </div>
+      <div>
+        <h2>Quiz Biologia</h2>
+      </div>
+      <div>
+        <h2>Pontuação:</h2>
+      </div>
+      <div>
+        <h2>User</h2>
+      </div>
+    </nav>
+  </div>
+  <div id="topo">
+    <div id="area-perguntas">
+      <div id="pergunta" v-for="(pergunta, index) in perguntas" :key="pergunta.name">
+        {{ pergunta.titulo }}
+        <div id="pergunta" v-for="(resposta, index) in pergunta.repostas" :key="resposta.name">
+          {{ resposta.letra }} - {{ resposta.texto }}
+        </div>
+      </div>
+     
+      <div><button>Enviar</button></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+nav {
+  background-color: #04acd6;
+}
+nav > div > h2 {
+  text-align: center;
+  color: rgb(0, 0, 0);
+}
+button {
+  border-radius: 20px;
+  background-color: #04acd6;
+  border-color: rgb(255, 255, 255);
+  color: white;
+  font-size: 25px;
+}
+body {
+  background-color: #10eedf5d;
+}
+#area-perguntas {
+  border-color: #04acd6;
+  border-style: solid;
+  background-color: #04acd694;
+  margin: 20px;
+  border-radius: 10px;
+  width: 800px;
+  font-size: 25px;
+}
+#pergunta {
+  color: rgb(0, 0, 0);
+  margin: 10px;
+  text-align: left;
+}
+#alternativas {
+  color: rgb(0, 0, 0);
+  margin: 10px;
+  text-align: left;
+}
+nav {
+  display: flex;
+  gap: 10%;
+  align-items: center;
+}
+nav > div > img {
+  margin-left: 10px;
+}
+
+#topo {
+  text-align: -webkit-center;
+}
+</style>
---
+++ src/assets/db.vue
@@ -0,0 +1,176 @@
+<script>
+export { perguntas }
+
+
+const perguntas = [
+     {  id: 1, 
+        titulo: "( Fuvest 2019 - 1ª fase) O processo de acidificação dos oceanos, decorrente das mudanças climáticas globais, afeta diretamente as colônias de corais, influenciando na formação de recifes. Assinale a alternativa que completa corretamente a explicação para esse fenômeno. O dióxido de carbono dissolvido no oceano",
+        dica: "Acidificação dos Oceanos O que causa a acidificação dos oceanos?A acidificação começou desde a primeira revolução industrial, em meados do século XVIII, quando a emissão de poluentes aumentou rápida e significativamente graças à instalação das indústrias por toda Europa. Como a escala de pH é logarítmica, uma leve diminuição neste valor pode representar, em porcentagem, variações de acidez de grandes dimensões. Dessa forma, é possível dizer que desde a primeira revolução industrial a acidez dos oceanos já aumentou em 30%. ECYCLE(2023) De acordo com estudos, cerca de 30% do dióxido de carbono (CO2) emitido pela atividade humana ao longo da história foi absorvido pelos oceanos. Quando o CO2 entra em contato com a água (H2O), é formado o ácido carbônico (H2CO3), que se dissocia no mar em íons de carbonato (CO32-) e hidrogênio (H+), resultando no aumento da acidez do oceano. “O nível de acidez se dá por meio do aumento da quantidade de íons H+ presentes em uma solução – nesse caso, a água do mar. Quanto maiores as emissões, maior a quantidade de íons H+ que se formam e mais ácidos os oceanos ficam.“ ECYCLE(2023)",
+
+        Referências: "ECYCLE. O que causa a acidificação dos oceanos?. Disponível em: https://www.ecycle.com.br/acidificacao-dos-oceanos/. Acesso em:13/03/2023.",
+        
+        repostas: [{
+            letra: "A)",
+            texto: "Gera menor quantidade de íons de hidrogênio, o que diminui o pH da água, liberando maior quantidade de íons cálcio, que, por sua vez, se ligam aos carbonatos, aumentando o tamanho dos recifes.",
+            correta: "no"
+        },
+        {
+            letra: "B",
+            texto: "É absorvido pelo fitoplâncton, entrando no processo fotossintético, e o oxigênio liberado permanece na água do mar, oxidando e matando os recifes de coral.",
+            correta: "no"
+        },
+        {
+            letra: "C",
+            texto: "Leva à formação de ácido carbônico, que, dissociado, gera, ao final, íons de hidrogênio e de carbonato, que se ligam, impedindo a formação do carbonato de cálcio que compõe os recifes de coral.",
+            correta: "no"
+        },
+        {
+            letra: "D",
+            texto: "É absorvido pelo fitoplâncton, entrando no processo fotossintético, e o oxigênio liberado torna a água do mar mais oxigenada, aumentando a atividade dos corais e o tamanho de seus recifes.",
+            correta: "no"
+        },
+        {
+            letra: "E)",
+            texto: "Reage com a água, produzindo ácido carbônico, que permanece no oceano e corrói os recifes de coral, que são formados por carbonato de cálcio.",
+            correta: "yes"
+        }]
+    },
+    {  id: 2, 
+        titulo: "(ENEM - 2016 - 2ª aplicação) Em uma aula de biologia sobre formação vegetal brasileira, a professora destacou que, em uma região, a flora convive com condições ambientais curiosas. As características dessas plantas não estão relacionadas com a falta de água, mas com as condições do solo, que é pobre em sais minerais, ácido e rico em alumínio. Além disso, essas plantas possuem adaptações ao fogo. As características adaptativas das plantas que correspondem à região destacada pela professora são:",
+
+        dica: "Como dito por Lauredo (2023), Raízes Aéreas são raízes que se desenvolvem em algum tipo de suporte. Elas podem ser encontradas em plantas epífitas (que vivem sobre outros vegetais), como as orquídeas. É importante lembrar que esse tipo de raiz não é parasita e não causa nenhum tipo de prejuízo para a planta suporte. (LAUREDO, 2023) Raízes Tabulares: são um tipo de raiz suporte que se destacam por sua aparência semelhante a grandes tábuas. Elas desempenham um papel importante no aumento da estabilidade da planta e também ajudam na aeração do solo. Esse tipo de raiz é mais comum em árvores de grande porte, como o chichá, onde sua presença é essencial para garantir o equilíbrio e a sustentação da planta em solos de baixa qualidade ou em terrenos inclinados. De acordo com Brasil escola (2023), raízes escoras são raízes aéreas que apresentam a função de suporte. São produzidas nos caules e ramos de muitas árvores, sendo, portanto, raízes adventícias. São encontradas em plantas altas e pesadas na sua parte superior, como o milho.(BRASIL ESCOLA, 2023) Raízes Respiratórias: São raízes que crescem em direção à superfície e fornecem oxigênio às partes submersas. Apresentam orifícios chamados pnemátodos em toda a sua expansão. Estas raízes atingem até o nível das marés mais altas. É comum encontrarmos esse tipo de raiz em manguezais. (EDUCA MAIS BRASIL, 2023) Folhas Largas: as dicotiledôneas são conhecidas como plantas de folhas largas e as monocotiledôneas como plantas de folhas estreitas ou gramíneas. As dicotiledôneas possuem dois cotilédones (estruturas de reserva de nutrientes) em suas sementes, enquanto as monocotiledôneas possuem apenas um. Casca grossa: De acordo com a explicação fornecida pela OpenAI (2023), a expressão casca grossa na botânica refere-se à espessura da camada externa de proteção de certas plantas, como troncos de árvores e caules de algumas espécies vegetais. Galhos Retorcidos: isso geralmente acontece na região do cerrado onde acontecem muitos incêndios e o alto nível de alumínio acaba provocando elevação da acidez no solo, isso faz com que diminua a taxa de nutrientes assim tornando tóxico para as plantas que não são adaptadas.",
+
+        Referências: "LAUREDO, Paula. Tipos de Raízes. Disponível em: https://escolakids.uol.com.br/ciencias/tipos-de-raizes.htm. Acessado em: 13/03/2023. BRASIL ESCOLA. Tipos especiais de raiz. Disponível em: https://brasilescola.uol.com.br/biologia/raiz.htm. Acesso em: 13/03/2023.EDUCA MAIS BRASIL. Tipos de Raízes. Disponível em: https://www.educamaisbrasil.com.br/enem/biologia/tipos-de-raizes. Acessado em: 18/03/2023.",
+        
+        repostas: [{
+            letra: "A)",
+            texto: "Raízes escoras e respiratórias.",
+            correta: "no"
+        },
+        {
+            letra: "B",
+            texto: "Raízes tabulares e folhas largas",
+            correta: "no"
+        },
+        {
+            letra: "C",
+            texto: "Casca grossa e galhos retorcidos",
+            correta: "yes"
+        },
+        {
+            letra: "D",
+            texto: "Raízes aéreas e perpendiculares ao solo",
+            correta: "no"
+        }]
+
+    },
+    {  id: 3, 
+        titulo: "((UFMG) Se o total de bases nitrogenadas de uma sequência de DNA de fita dupla é igual a 240, e nela existirem 30% de adenina, o número de moléculas de guanina será:",
+
+        dica: "Adenina, Timina, Guanina e Citosina De acordo com o Blog do Vestibular (2023), no DNA, a porcentagem de timina é igual à de adenina, enquanto a porcentagem de citosina é igual à de guanina. Isso acontece devido ao pareamento das bases (A = T; C=G). (BLOG DO VESTIBULAR, 2023) Adenina: É uma base nitrogenada que formam o RNA e o DNA, ou seja, são responsáveis pela configuração. E faz “dupla” com a Timina. Citosina: Assim como a adenina, também é uma base nitrogenada que forma o RNA e o DNA, mas o diferencial entre elas é que a citosina faz “dupla” com a Guanina.",
+
+        Referências: "BLOG DO VESTIBULAR. Questão comentada sobre análise bioquímica de DNA. Disponível em: https://www.blogdovestibular.com/enem-2016/questao-comentada-sobre-analise-bioquimica-de-dna.html#:~:text=No%20DNA%2C%20a%20porcentagem%20de,e%2015%25%20s%C3%A3o%20de%20guanina. Acesso em: 19/03/2023.",
+        
+        repostas: [{
+            letra: "A)",
+            texto: "48",
+            correta: "no"
+        },
+        {
+            letra: "B)",
+            texto: "72",
+            correta: "yes"
+        },
+        {
+            letra: "C)",
+            texto: "120",
+            correta: "no"
+        },
+        {
+            letra: "D)",
+            texto: "144",
+            correta: "no"
+        },
+        {
+            letra: "E)",
+            texto: "168",
+            correta: "no"
+        }]
+
+    },
+    {  id: 4, 
+        titulo: "(Lista de exercícios, Brasil Escola) A estrutura do DNA atualmente aceita, conhecida como modelo de dupla hélice, foi proposto por quais pesquisadores?",
+
+        dica: "A descoberta do DNA, do formato de sua molécula e do mecanismo de ação dos genes figuram entre os maiores avanços científicos já registrados. Tanto que o prêmio Nobel de Medicina já foi concedido a vários cientistas por esmiuçarem este assunto, como James Watson e Francis Crick, que há 51 anos ganharam o prêmio por descreverem a estrutura do DNA. (BLOG DO ENEM, 2023)",
+
+        Referências: "BLOG DO VESTIBULAR. Questão comentada sobre análise bioquímica de DNA. Disponível em: https://www.blogdovestibular.com/enem-2016/questao-comentada-sobre-analise-bioquimica-de-dna.html#:~:text=No%20DNA%2C%20a%20porcentagem%20de,e%2015%25%20s%C3%A3o%20de%20guanina. Acesso em: 19/03/2023.",
+
+        imagem: "https://i0.wp.com/blog.meudna.com/wp-content/uploads/2021/11/Ab64-corpo.jpg?resize=1024%2C710&ssl=1",
+        
+        repostas: [{
+            letra: "A)",
+            texto: "Darwin e Wallace",
+            correta: "no"
+        },
+        {
+            letra: "B)",
+            texto: "Watson e Crick",
+            correta: "yes"
+        },
+        {
+            letra: "C)",
+            texto: "Pierre e Marie Curie",
+            correta: "no"
+        },
+        {
+            letra: "D)",
+            texto: "Mendel e Wallace",
+            correta: "no"
+        },
+        {
+            letra: "E)",
+            texto: "Newton e Einstein",
+            correta: "no"
+        }]
+
+    },
+    {  id: 5, 
+        titulo: "(Fuvest) Uma ideia comum às teorias da evolução propostas por Darwin e por Lamarck é que a adaptação resulta :",
+
+        dica: "Segundo Silva e Pignata (2014) a Teoria da Evolução de Darwin afirma que é o ambiente, por meio de seleção natural, que determina a importância da característica do indivíduo ou de suas variações, e os organismos mais bem adaptados a esse ambiente têm maiores chances de sobrevivência, deixando um número maior de descendentes. Mas, de acordo com Lamarck, todas as mudanças estruturais sofridas por um indivíduo seriam transmitidas para seus descendentes. Essa teoria explicaria, por exemplo, o alongamento do pescoço das girafas e a cegueira das toupeiras. Essa ideia ficou conhecida como a Lei da Herança dos Caracteres Adquiridos de Lamarck.",
+
+        Referências: "SILVA, Ricardo Fernandes da e PIGNATA, Maria Izabel Bernez. CHARLES DARWIN E A TEORIA DA EVOLUÇÃO. Disponível em: https://files.cercomp.ufg.br/weby/up/80/o/TCEM2014-Biologia-RicardoFernandesSilva.pdf. Acesso em:19/03/2023.",
+        
+        repostas: [{
+            letra: "A)",
+            texto: "Do sucesso reprodutivo diferencial",
+            correta: "no"
+        },
+        {
+            letra: "B)",
+            texto: "De uso e desuso de estruturas anatômicas",
+            correta: "no"
+        },
+        {
+            letra: "C)",
+            texto: "Da interação entre os organismos e seus ambientes",
+            correta: "yes"
+        },
+        {
+            letra: "D)",
+            texto: "Da manutenção das melhores combinações gênicas",
+            correta: "no"
+        },
+        {
+            letra: "E)",
+            texto: "De mutações gênicas induzidas pelo ambiente",
+            correta: "no"
+        }]
+
+    },
+        
+        
+]
+
+
+</script>
